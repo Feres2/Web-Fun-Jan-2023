c1ee18669536f2fddeb3e19124e1528dd1b6258d
Feres2 <feresalyani@gmail.com>
2023-01-23 15:36:06 +0100

    pizza

---
+++ Week1/Day 5/Practice/pizza.js
@@ -0,0 +1,20 @@
+
+function pizzaOven(crustType,sauceType,cheeses,toppings){
+    var pizza={};
+    pizza.crustType = crustType;
+    pizza.sauceType= sauceType;
+    pizza.cheeses=cheeses;
+    pizza.toppings=toppings;
+    return pizaa;
+    
+}
+var p1 = pizzaOven ("deep dish","traditional","mozzarella",["pepperoni","sausage"]);
+var p2 = pizzaOven ("hand tossed","marinara",["mozzarella","feta"],["mushrooms","olives","onions"]);
+var p3 = pizzaOven ("wood tossted","blanca sause","rockford","bacon");
+var p4 = pizaaOven ("deep dish","marinara","feta",["olives","tuna"]);
+var p5 = pizaaOven ("deep dish","blanca sause","goat cheese",["olives","tuna","bacon"]);
+console.log(p1);
+console.log(p2);
+console.log(p3);
+console.log(p4);
+console.log(p5);
