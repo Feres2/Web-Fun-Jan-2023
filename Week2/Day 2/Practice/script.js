e31da208b5906ad53979361d0d8f15049a1b26b9
Feres2 <feresalyani@gmail.com>
2023-01-24 21:27:35 +0100

    profillee

---
+++ Week2/Day 2/Practice/script.js
@@ -0,0 +1,21 @@
+console.log("page loaded...");
+var element = document.querySelector("#joe");
+var e = document.querySelector(".rem");
+var ee = document.querySelector(".rim");
+ 
+
+function edit(){
+    element.innerText=("Kim Kardashen");
+    
+    
+}
+
+function acceptNot(){
+    e.remove();
+}
+
+function acceptNott(){
+    ee.remove();
+    count--
+    count.innerText
+}
