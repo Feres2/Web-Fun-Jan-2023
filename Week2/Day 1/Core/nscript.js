0cc8077cc15e069bf4c099d3731977f5250dca59
Feres2 <feresalyani@gmail.com>
2023-01-24 20:11:05 +0100

    like

---
+++ Week2/Day 1/Core/nscript.js
@@ -0,0 +1,29 @@
+var like1=9;
+var res=document.querySelector("#lik1")
+function like(){
+    like1++;
+
+    res.innerText= (like1 + "like(s)") 
+    
+
+}
+var like2=12;
+var ress=document.querySelector("#lik2")
+function likes(){
+    like2++;
+
+    ress.innerText= (like2 + "like(s)") 
+    
+
+}
+var like3=9;
+var resss=document.querySelector("#lik3")
+function likess(){
+    like3++;
+
+    resss.innerText= (like3 + "like(s)") 
+    
+
+}
+
+
---
+++ Week2/Day 1/Core/script.js
@@ -0,0 +1,9 @@
+var likes=9;
+var res=document.querySelector("#lik1")
+function like(){
+    likes++;
+
+    res.innerHTML= (likes + "like(s)") 
+    
+
+}
