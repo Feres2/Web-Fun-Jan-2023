db86d65d0bef0d9b9071bf6d5a535a6607332018
Feres2 <feresalyani@gmail.com>
2023-01-26 20:31:28 +0100

    weather

---
+++ Week2/Day 4/Practice/script.js
@@ -0,0 +1,12 @@
+var e=document.querySelector(".nav-links");
+var c=document.querySelector(".cookie")
+
+
+
+function loading(){
+    alert("Loading weather report...");
+
+}
+function cancel(){
+    c.remove();
+}
